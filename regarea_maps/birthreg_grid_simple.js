180ca0329800f7777efdb4503b8859495a1c3fa2
Shane Landrum <srl@cliotropic.org>
2011-03-18 22:47:13 -0400

    Added protovis registration-areas maps demo

---
+++ regarea_maps/birthreg_grid_simple.js
@@ -0,0 +1,367 @@
+var birthreg = {
+ 'AL':  {
+	 state: 'ALABAMA',
+	 abbrev: 'AL',
+	 year_entered_birth_reg_area: 1927,
+
+	 year_entered_death_reg_area: 1925
+	},
+'AK':  {
+	 state: 'ALASKA',
+	 abbrev: 'AK',
+	 year_entered_birth_reg_area: 1949,
+
+	 year_entered_death_reg_area: 1949
+	},
+'AZ':  {
+	 state: 'ARIZONA',
+	 abbrev: 'AZ',
+	 year_entered_birth_reg_area: 1926,
+
+	 year_entered_death_reg_area: 1926
+	},
+'AR':  {
+	 state: 'ARKANSAS',
+	 abbrev: 'AR',
+	 year_entered_birth_reg_area: 1927,
+
+	 year_entered_death_reg_area: 1927
+	},
+'CA':  {
+	 state: 'CALIFORNIA',
+	 abbrev: 'CA',
+	 year_entered_birth_reg_area: 1919,
+
+	 year_entered_death_reg_area: 1906
+	},
+'CO':  {
+	 state: 'COLORADO',
+	 abbrev: 'CO',
+	 year_entered_birth_reg_area: 1928,
+
+	 year_entered_death_reg_area: 1906
+	},
+'CT':  {
+	 state: 'CONNECTICUT',
+	 abbrev: 'CT',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1890
+	},
+'DE':  {
+	 state: 'DELAWARE',
+	 abbrev: 'DE',
+	 year_entered_birth_reg_area: 1921,
+
+	 year_entered_death_reg_area: 1919
+	},
+'DC':  {
+	 state: 'DIST. OF COLUMBIA',
+	 abbrev: 'DC',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1880
+	},
+'FL': {
+	 state: 'FLORIDA',
+	 abbrev: 'FL',
+	 year_entered_birth_reg_area: 1924,
+
+	 year_entered_death_reg_area: 1919
+	},
+'GA':  {
+	 state: 'GEORGIA',
+	 abbrev: 'GA',
+	 year_entered_birth_reg_area: 1928,
+
+	 year_entered_death_reg_area: 1928
+	},
+'HI':  {
+	 state: 'HAWAII',
+	 abbrev: 'HI',
+	 year_entered_birth_reg_area: 1949,
+
+	 year_entered_death_reg_area: 1949
+	},
+'ID':  {
+	 state: 'IDAHO',
+	 abbrev: 'ID',
+	 year_entered_birth_reg_area: 1926,
+
+	 year_entered_death_reg_area: 1922
+	},
+'IL':  {
+	 state: 'ILLINOIS',
+	 abbrev: 'IL',
+	 year_entered_birth_reg_area: 1922,
+
+	 year_entered_death_reg_area: 1918
+	},
+'IN':  {
+	 state: 'INDIANA',
+	 abbrev: 'IN',
+	 year_entered_birth_reg_area: 1917,
+
+	 year_entered_death_reg_area: 1900
+	},
+'IA':  {
+	 state: 'IOWA',
+	 abbrev: 'IA',
+	 year_entered_birth_reg_area: 1924,
+
+	 year_entered_death_reg_area: 1920
+	},
+'KS':  {
+	 state: 'KANSAS',
+	 abbrev: 'KS',
+	 year_entered_birth_reg_area: 1917,
+
+	 year_entered_death_reg_area: 1914
+	},
+'KY':  {
+	 state: 'KENTUCKY',
+	 abbrev: 'KY',
+	 year_entered_birth_reg_area: 1917,
+
+	 year_entered_death_reg_area: 1911
+	},
+'LA':  {
+	 state: 'LOUISIANA',
+	 abbrev: 'LA',
+	 year_entered_birth_reg_area: 1927,
+
+	 year_entered_death_reg_area: 1918
+	},
+'ME':  {
+	 state: 'MAINE',
+	 abbrev: 'ME',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1900
+	},
+'MD':  {
+	 state: 'MARYLAND',
+	 abbrev: 'MD',
+	 year_entered_birth_reg_area: 1916,
+
+	 year_entered_death_reg_area: 1906
+	},
+'MA':  {
+	 state: 'MASSACHUSETTS',
+	 abbrev: 'MA',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1880
+	},
+'MI':  {
+	 state: 'MICHIGAN',
+	 abbrev: 'MI',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1900
+	},
+'MN':  {
+	 state: 'MINNESOTA',
+	 abbrev: 'MN',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1910
+	},
+'MS':  {
+	 state: 'MISSISSIPPI',
+	 abbrev: 'MS',
+	 year_entered_birth_reg_area: 1921,
+
+	 year_entered_death_reg_area: 1919
+	},
+'MO':  {
+	 state: 'MISSOURI',
+	 abbrev: 'MO',
+	 year_entered_birth_reg_area: 1927,
+
+	 year_entered_death_reg_area: 1911
+	},
+'MT':  {
+	 state: 'MONTANA',
+	 abbrev: 'MT',
+	 year_entered_birth_reg_area: 1922,
+
+	 year_entered_death_reg_area: 1910
+	},
+'NE':  {
+	 state: 'NEBRASKA',
+	 abbrev: 'NE',
+	 year_entered_birth_reg_area: 1929,
+
+	 year_entered_death_reg_area: 1920
+	},
+'NV':  {
+	 state: 'NEVADA',
+	 abbrev: 'NV',
+	 year_entered_birth_reg_area: 1929,
+
+	 year_entered_death_reg_area: 1929
+	},
+'NH':  {
+	 state: 'NEW HAMPSHIRE',
+	 abbrev: 'NH',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1890
+	},
+'NJ':  {
+	 state: 'NEW JERSEY',
+	 abbrev: 'NJ',
+	 year_entered_birth_reg_area: 1921,
+
+	 year_entered_death_reg_area: 1880
+	},
+'NM':  {
+	 state: 'NEW MEXICO',
+	 abbrev: 'NM',
+	 year_entered_birth_reg_area: 1929,
+
+	 year_entered_death_reg_area: 1929
+	},
+'NY':  {
+	 state: 'NEW YORK',
+	 abbrev: 'NY',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1890
+	},
+'NC':  {
+	 state: 'NORTH CAROLINA',
+	 abbrev: 'NC',
+	 year_entered_birth_reg_area: 1917,
+
+	 year_entered_death_reg_area: 1916
+	},
+'ND':  {
+	 state: 'NORTH DAKOTA',
+	 abbrev: 'ND',
+	 year_entered_birth_reg_area: 1924,
+
+	 year_entered_death_reg_area: 1924
+	},
+'OH':  {
+	 state: 'OHIO',
+	 abbrev: 'OH',
+	 year_entered_birth_reg_area: 1917,
+
+	 year_entered_death_reg_area: 1909
+	},
+'OK':  {
+	 state: 'OKLAHOMA',
+	 abbrev: 'OK',
+	 year_entered_birth_reg_area: 1928,
+
+	 year_entered_death_reg_area: 1928
+	},
+'OR':  {
+	 state: 'OREGON',
+	 abbrev: 'OR',
+	 year_entered_birth_reg_area: 1919,
+
+	 year_entered_death_reg_area: 1918
+	},
+'PA':  {
+	 state: 'PENNSYLVANIA',
+	 abbrev: 'PA',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1906
+	},
+'RI':  {
+	 state: 'RHODE ISLAND',
+	 abbrev: 'RI',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1890
+	},
+'SC':  {
+	 state: 'SOUTH CAROLINA',
+	 abbrev: 'SC',
+	 year_entered_birth_reg_area: 1928,
+
+	 year_entered_death_reg_area: 1916
+	},
+'SD':  {
+	 state: 'SOUTH DAKOTA',
+	 abbrev: 'SD',
+	 year_entered_birth_reg_area: 1932,
+
+	 year_entered_death_reg_area: 1930
+	},
+'TN':  {
+	 state: 'TENNESSEE',
+	 abbrev: 'TN',
+	 year_entered_birth_reg_area: 1927,
+
+	 year_entered_death_reg_area: 1917
+	},
+'TX':  {
+	 state: 'TEXAS',
+	 abbrev: 'TX',
+	 year_entered_birth_reg_area: 1933,
+
+	 year_entered_death_reg_area: 1933
+	},
+'UT':  {
+	 state: 'UTAH',
+	 abbrev: 'UT',
+	 year_entered_birth_reg_area: 1917,
+
+	 year_entered_death_reg_area: 1910
+	},
+'VT':  {
+	 state: 'VERMONT',
+	 abbrev: 'VT',
+	 year_entered_birth_reg_area: 1915,
+
+	 year_entered_death_reg_area: 1890
+	},
+'VA':  {
+	 state: 'VIRGINIA',
+	 abbrev: 'VA',
+	 year_entered_birth_reg_area: 1917,
+
+	 year_entered_death_reg_area: 1913
+	},
+'WA':  {
+	 state: 'WASHINGTON',
+	 abbrev: 'WA',
+	 year_entered_birth_reg_area: 1917,
+
+	 year_entered_death_reg_area: 1908
+	},
+'WV':  {
+	 state: 'WEST VIRGINIA',
+	 abbrev: 'WV',
+	 year_entered_birth_reg_area: 1925,
+
+	 year_entered_death_reg_area: 1925
+	},
+'WI':  {
+	 state: 'WISCONSIN',
+	 abbrev: 'WI',
+	 year_entered_birth_reg_area: 1917,
+
+	 year_entered_death_reg_area: 1908
+	},
+'WY':  {
+	 state: 'WYOMING',
+	 abbrev: 'WY',
+	 year_entered_birth_reg_area: 1922,
+
+	 year_entered_death_reg_area: 1922
+	}
+}; 
+
+        
+birthreg.maxYear = 1933;
+birthreg.minYear = 1903;
+birthreg.startYear = 1903;
+birthreg.colorThis = function(thisYear, yearEntered) {
+	return (thisYear >= yearEntered);	
+}
